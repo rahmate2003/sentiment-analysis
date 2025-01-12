93640d7ef58d7cf12a49f7cfb14a99f7c6ccdd96
rahmate2003 <w.rahmad26@gmail.com>
2025-01-12 21:36:14 +0800

    API

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
   }[label] || label);
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 py-3 flex flex-col justify-center sm:py-6">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
