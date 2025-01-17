b84df62e85fa2ba2861b2a01d93f4d30751132e0
rahmate2003 <w.rahmad26@gmail.com>
2025-01-17 21:29:54 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api-sentiment-analysis-gamma.vercel.app/',  // Menggunakan URL yang sudah didefinisikan
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Menghapus /api prefix jika diperlukan
-      },
-    },
-  },
+  plugins: [react()]
 });
